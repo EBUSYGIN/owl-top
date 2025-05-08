62c5140d236c8628d2b49e67c9c6ebce109005e9
Egor Busygin <e.busygin2003@gmail.com>
2025-05-08 11:08:44 +0300

    .

--- src/widgets/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/Sidebar.tsx
@@ -8,6 +8,15 @@ import { SidebarProps } from './Sidebar.props';
 export function Sidebar({ secondLevelMenuItems }: SidebarProps) {
   return (
     <div className={styles.sidebar}>
+      <div className={styles.logoBox}>
+        <Icon.Logo />
+        <Title tag='h1' appearance='bold'>
+          OWL
+        </Title>
+        <Title tag='h1' appearance='thin'>
+          top
+        </Title>
+      </div>
       <Search />
       <Menu secondLevelMenuItems={secondLevelMenuItems} />
     </div>
