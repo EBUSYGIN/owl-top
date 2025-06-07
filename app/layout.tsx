a1b1cab36415bf46ff0f53056e249ab426a3ecc0
Egor <e.busygin2003@gmail.com>
2025-06-07 20:26:57 +0300

    minor fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,11 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Courses Top",
-    default: "Courses Top",
+    template: "%s | OWL Top",
+    default: "OWL Top",
   },
   description:
-    "Courses Top — платформа для оценки и рейтинга курсов. Читайте отзывы, ставьте оценки, анализируйте плюсы и минусы. Выбирайте лучшие курсы на основе мнений студентов!",
+    "OWL Top — платформа для оценки и рейтинга курсов. Читайте отзывы, ставьте оценки, анализируйте плюсы и минусы. Выбирайте лучшие курсы на основе мнений студентов!",
 };
 
 export default function RootLayout({
